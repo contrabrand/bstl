33435fb3f24dc45a6398e149c1e69908dbf93633
contrabrand <86887091+contrabrand@users.noreply.github.com>
2022-02-16 07:03:55 +0800

    Update apup.php

--- apup.php
+++ apup.php
@@ -1,1 +1,57 @@
 
+{"Version":"1.01",
+
+"Url":"https://github.com/contrabrand/bstl/raw/main/stl.zip" ,
+
+"Changelog":"  
+
+        🙏 WELCOME TO Stallion OVPN 🙏
+
+            Fast  Secure Stable
+
+New updated 16/02/2022
+
+
+Stallion OVPN is a free  app for those who want to access internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🟢 AIS 🇹🇭 
+🔵 DTAC 🇹🇭
+🔴 TRUE 🇹🇭
+🔴 MAXIS 🇲🇾 🔜
+🟡 DIGI 🇲🇾 🔜
+🟠 Umobile 🇲🇾 🔜
+🔵 Celcom 🇲🇾 🔜
+🔴 Telkomsel 🇲🇨 🔜
+
+
+🇸🇬 
+🇹🇭 
+🇲🇾 🔜
+🇲🇨 🔜
+🇭🇰 🔜
+🇳🇱
+🇮🇳
+🇺🇸
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
